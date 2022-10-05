3d1fa081ea97d6a855cfc6ed4776bb01db64b247
xdianovx <dianov.js@gmail.com>
2022-10-05 14:55:49 +0300

    asd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     return (
         <header className={s.header}>
             <Button onClick={onClose()}>Закрыть</Button>
-            <span></span>
+            <span>asd</span>
         </header>
     );
 };
