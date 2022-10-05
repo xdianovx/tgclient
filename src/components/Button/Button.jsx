a0eb04b3edc4a7b35dcc2b28322def93d4d462b0
xdianovx <dianov.js@gmail.com>
2022-10-05 14:59:04 +0300

    asd

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './button.module.css'
 
-const Button = (children, props) => {
+const Button = ({children, props}) => {
     return (
         <button className={s.btn}  {...props}>
             {children}
