be92a5b7adb5538693b13d5a12b58f946ed8cdb4
xdianovx <dianov.js@gmail.com>
2022-10-05 15:12:42 +0300

    Username

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Button from "./components/Button/Button";
+import {useTg} from "./hooks/useTg";
+import {useEffect} from "react";
 
 
 function App() {
+    const {onToggleBtn, tg} = useTg()
 
+    useEffect(() => {
+        tg.ready()
+    }, [])
+    
     return (
         <div className="App">
-            <Button>Кнопка</Button>
+            <Button onClick={onToggleBtn}> Toggle</Button>
         </div>
     );
 }
